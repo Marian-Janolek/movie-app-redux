56e3d0bf7946c2568265928035e05bcafb084b8e
Marian-Janolek <m.janolek@gmail.com>
2022-02-11 14:01:50 +0100

    add redux , header, footer, movieCard, movieListing and fetch movies

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,34 @@
-import React from 'react';
+import axios from 'axios';
+import { useEffect } from 'react';
+import MovieListing from '../MovieListing/MovieListing';
+import movieApi from '../../common/apis/movieApi';
+import { addMovies } from '../../features/movies/movieSlice';
+import { useDispatch } from 'react-redux';
 
 const Home = () => {
-  return <div>Home</div>;
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const fetchMovies = async () => {
+      const movieText = 'Harry';
+      try {
+        const { data } = await movieApi.get(
+          `?apikey=${process.env.REACT_APP_OMDB_API_KEY}&s=${movieText}&type=movie`
+        );
+        dispatch(addMovies(data));
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchMovies();
+  }, []);
+
+  return (
+    <div>
+      <div className="banner-image">
+        <MovieListing />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
