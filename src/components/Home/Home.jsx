cf44ef68c197ea2dd314addc1920249d2990336c
Marian-Janolek <m.janolek@gmail.com>
2022-02-11 17:27:01 +0100

    add carousel single movie and show page , search

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,26 +1,20 @@
-import axios from 'axios';
 import { useEffect } from 'react';
 import MovieListing from '../MovieListing/MovieListing';
-import movieApi from '../../common/apis/movieApi';
-import { addMovies } from '../../features/movies/movieSlice';
 import { useDispatch } from 'react-redux';
+import {
+  fetchAsyncMovies,
+  fetchAsyncShows,
+} from '../../features/movies/movieSlice';
 
 const Home = () => {
   const dispatch = useDispatch();
+  const movieText = 'Harry';
+  const showText = 'Friends';
+
   useEffect(() => {
-    const fetchMovies = async () => {
-      const movieText = 'Harry';
-      try {
-        const { data } = await movieApi.get(
-          `?apikey=${process.env.REACT_APP_OMDB_API_KEY}&s=${movieText}&type=movie`
-        );
-        dispatch(addMovies(data));
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    fetchMovies();
-  }, []);
+    dispatch(fetchAsyncMovies(movieText));
+    dispatch(fetchAsyncShows(showText));
+  }, [dispatch]);
 
   return (
     <div>
--- src/features/movies/movieSlice.js
+++ src/features/movies/movieSlice.js
@@ -1,20 +1,75 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import movieApi from '../../common/apis/movieApi';
 
+export const fetchAsyncMovies = createAsyncThunk(
+  'movies/fetchAsyncMovies',
+  async (term) => {
+    const { data } = await movieApi.get(
+      `?apikey=${process.env.REACT_APP_OMDB_API_KEY}&s=${term}&type=movie`
+    );
+    return data;
+  }
+);
+
+export const fetchAsyncShows = createAsyncThunk(
+  'movies/fetchAsyncShows',
+  async (term) => {
+    const { data } = await movieApi.get(
+      `?apikey=${process.env.REACT_APP_OMDB_API_KEY}&s=${term}&type=series`
+    );
+    return data;
+  }
+);
+
+export const fetchAsyncMovieOrShowDetail = createAsyncThunk(
+  'movies/fetchAsyncMovieOrShowDetail',
+  async (id) => {
+    const { data } = await movieApi.get(
+      `?apikey=${process.env.REACT_APP_OMDB_API_KEY}&i=${id}&Plot=full`
+    );
+    return data;
+  }
+);
 const initialState = {
   movies: {},
+  shows: {},
+  selectedMovieOrShow: {},
 };
 
 const movieSlice = createSlice({
   name: 'movies',
   initialState,
   reducers: {
-    addMovies: (state, { payload }) => {
-      state.movies = payload;
+    removeSelectedMovieOrShow: (state) => {
+      state.selectedMovieOrShow = {};
+    },
+  },
+  extraReducers: {
+    [fetchAsyncMovies.pending]: () => {
+      console.log('pending');
+    },
+    [fetchAsyncMovies.fulfilled]: (state, { payload }) => {
+      console.log('fetched successfully');
+      return { ...state, movies: payload };
+    },
+    [fetchAsyncShows.fulfilled]: (state, { payload }) => {
+      console.log('fetched successfully');
+      return { ...state, shows: payload };
+    },
+    [fetchAsyncMovieOrShowDetail.fulfilled]: (state, { payload }) => {
+      console.log('fetched successfully');
+      return { ...state, selectedMovieOrShow: payload };
+    },
+    [fetchAsyncMovies.rejected]: () => {
+      console.log('rejected');
     },
   },
 });
 
-export const { addMovies } = movieSlice.actions;
+export const { removeSelectedMovieOrShow } = movieSlice.actions;
 export const getAllMovies = (state) => state.movies.movies;
+export const getAllShows = (state) => state.movies.shows;
+export const getSelectedMoviesOrShow = (state) =>
+  state.movies.selectedMovieOrShow;
 
 export default movieSlice.reducer;
